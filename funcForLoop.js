1b9f94bab69f7166f1d943dae455c2e14ac723d4
Bob Hunt <bob.hunt.email@gmail.com>
2018-03-23 15:52:04 -0400

    completed

--- funcForLoop.js
+++ funcForLoop.js
@@ -4,26 +4,28 @@
 // You may use built-in operators to finish the definition.
 // Display the result of calling add(2, 4), which should be 6.
 const num1 = document.createElement("div");
-function add(e,f){
-    return e+f;
+function add(e, f) {
+    return e + f;
     // console.log(add);
 }
-let e=2;
-let f=4;
-num1.textContent = add(e,f);
+
+// num1.textContent = add(e,f);
+num1.textContent = add(2, 4);
 document.body.appendChild(num1);
 
 // #2 Write a function named "multiply" that takes two arguments and returns their product.
 // You may not use built-in arithmetic operators or functions. Instead, you'll need a for loop which calls the "add" function you wrote earlier.
 // Display the result of calling multiply(6, 8), which should be 48.
 const num2 = document.createElement("div");
-let a=6;
-let b=8;
-let sum2=0;
-for (i=0; i<b; i++){
-    sum2=sum2+a;
+
+function multiply(a, b) {
+    let sum2 = 0;
+    for (let i = 0; i < b; i++) {
+        sum2 = add(sum2, a);
+    }
+    return sum2;
 }
-num2.textContent = sum2;
+num2.textContent = multiply(6, 8);
 document.body.appendChild(num2);
 // ** I don't understand why line 26 isn't
 // ** for (i=0; i<=b; i++){
@@ -32,17 +34,16 @@ document.body.appendChild(num2);
 // You may not use built-in arithmetic operators or functions. Instead, use functions you wrote in earlier katas to write this function.
 // Display the result of calling power(2, 8), which should be 256.
 const num3 = document.createElement("div");
-let x=2;
-let n=8;
-let product=1;
 
-function power(){
-product = product * x;
+function power(a, b) {
+    let sum2 = 1;
+    for (let i = 0; i < b; i++) {
+        sum2 = multiply(sum2, a);
+    }
+    return sum2;
 }
-for (i=0; i<n; i++){
-power();
-}
-num3.textContent = product;
+
+num3.textContent = power(2, 8);
 document.body.appendChild(num3);
 // ** I'm not sure how I came up with this, but it works! 
 
@@ -50,15 +51,15 @@ document.body.appendChild(num3);
 // You may not use built-in arithmetic operators or functions. Instead, use functions you wrote in earlier katas to write this function.
 // Display their result of calling factorial(4), which should be 24 (4 * 3 * 2 * 1).
 const num4 = document.createElement("div");
-let z=4;
-let prod4=1
-function factorial(){
-prod4 = prod4*i;
+
+function factorial(a) {
+    let fact = 1;
+    for (let i = 1; i <= a; i++) {
+        fact = multiply(fact, i);
+    }
+    return fact;
 }
-for (i=z; i>0; i--){
-factorial();
-}
-num4.textContent = prod4;
+num4.textContent = factorial(4);
 document.body.appendChild(num4);
 
 // #5 Write a function named "fibonacci" that takes an argument n and returns the nth, Fibonacci number (Links to an external site.)Links to an external site. (click me!).
@@ -67,30 +68,14 @@ document.body.appendChild(num4);
 // 0 1 1 2 3 5 8 [13] 21
 // The number in brackets is the 8th fibonacci number.
 const num5 = document.createElement("div");
-
-
 function fibonacci(n){
-fibNum = 0;
-newNum = 1;
-origNum = 0;
-    
-    if(n>2){
-        for (i=2; i<n; i++){
-            fibNum = origNum + newNum;
-            origNum = newNum;
-            newNum = fibNum;
-        }
-    }else if(n===2){
-        fibNum = 1;
-    }else{
-        fibNum = 0;
+    let arr = [0,1];
+    for (let i=2; i<n; i++){
+        arr.push(add(arr[i-1],arr[i-2]));
     }
-
-    num5.textContent = fibNum;
-document.body.appendChild(num5)
+    return arr[arr.length-1];
 }
-
-console.log(fibonacci(8)); 
-
+num5.textContent = fibonacci(8);
+document.body.appendChild(num5);
 
 
